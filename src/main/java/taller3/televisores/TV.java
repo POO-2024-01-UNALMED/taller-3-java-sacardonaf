1d6a650fff68dbddb9a445aaf14d8af651b7b624
sacardonaf <sacardonaf@unal.edu.co>
2024-02-26 16:51:23 -0500

    intento 9

--- src/main/java/taller3/televisores/TV.java
+++ src/main/java/taller3/televisores/TV.java
@@ -57,11 +57,11 @@ public class TV {
 		this.control=control;
 	}
 	
-	public static int getNumTV() {
+	public int getNumTV() {
 		return TV.numTV;
 	}
 	
-	public static void setNumTV(int numTV) {
+	public void setNumTV(int numTV) {
 		TV.numTV=numTV;
 	}
 	
